6a999308a4097a8c005314faac1f4a79c92839cf
adri4silva <adri4silva@gmail.com>
2017-04-24 19:36:29 +0200

    Clase main para instanciar las dos ventanas

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Adri
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Usuario u1 = new Usuario();
+        Devoluciones d1 = new Devoluciones();
+        d1.setVisible(true);
+    }
+}
